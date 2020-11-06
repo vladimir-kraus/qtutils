c7879e33d46a23b29947c266fd16066b66b10b24
Vladimir Kraus <vladimir-kraus@users.noreply.github.com>
2020-11-06 13:50:41 +0100

    Update singleton.h

--- qtutils/singleton.h
+++ qtutils/singleton.h
@@ -2,6 +2,28 @@
 
 #include <cassert>
 
+//
+// A templated singleton base class which can extend any other class by using multiple inheritance.
+// It uses CRTP pattern, i.e. the template parameter is the derived class itself.
+// It provides static method instance() which returns a pointer to the underlying object.
+// 
+// This implementation of singleton expects the user to explicitly instantiate it.
+// It can be destroyed and instantiated again.  But it is the user's responsibility 
+// to have only one instance at the same time (it is asserted in debug mode).
+// In other words, the Singleton class does not affect the lifetime of 
+// the object in any way, it relies solely on the user of this class.
+// All it does is to provide global access to the object with instance().
+// 
+
+//
+// Usage:
+//
+// class CentralWidget : public Widget, public Singleton<CentralWidget>
+// { 
+// ... 
+// };
+// 
+
 template <typename T>
 class Singleton
 {
